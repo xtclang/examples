85c09a262ba4454f88ded55fb0c02919242c4cf4
Gene Gleyzer <gene@xqiz.it>
2022-05-25 15:03:52 -0400

    Fix the build

--- welcome/build.gradle.kts
+++ welcome/build.gradle.kts
@@ -8,20 +8,18 @@ version = "0.1.0"
 val server = project(":server");
 val webapp = project(":webapp");
 
+val webappDir = "${webapp.projectDir}"
+val serverDir = "${server.projectDir}"
+
+val webContent = "$serverDir/main/resources/webapp"
+val xdkExe     = "$serverDir/xdk/bin"
+
 tasks.register("clean") {
     group       = "Build"
     description = "Delete previous build results"
 
     dependsOn(server.tasks["clean"])
     dependsOn(webapp.tasks["clean"])
-}
-
-val build = tasks.register("build") {
-    group       = "Build"
-    description = "Build all"
-
-    dependsOn(webapp.tasks["build"])
-    dependsOn(server.tasks["build"])
 }
 
 tasks.register("upload") {
@@ -31,15 +29,153 @@ tasks.register("upload") {
     dependsOn(build)
 
     doLast {
-        var devHome  = file("${rootProject.projectDir}/../..")
+        var devHome  = file("${projectDir}/../..")
         val stageDir = "$devHome/staging/platform"
         val account  = "acme"
         val libDir   = "$stageDir/$account/lib"
 
-        println("Copying modules from ${server.buildDir} to $libDir")
+        println("Copying modules from ${serverDir}/build to $libDir")
         copy {
-            from("${server.buildDir}")
+            from("${serverDir}/build")
             into(libDir)
         }
+    }
+}
+val build = tasks.register("build") {
+    group       = "Build"
+    description = "Build all"
+
+    dependsOn(buildServer)
+}
+
+// ----- server build tasks ------------------------------------------------------------------------
+
+val buildServer = tasks.register("buildServer") {
+    group       = "Build"
+    description = "Compile server modules"
+
+    dependsOn(compileWelcome)
+}
+
+val compileWelcome = tasks.register("compileWelcome") {
+    group       = "Build"
+    description = "Compile welcome module"
+
+    dependsOn(compileWelcomeDB)
+    dependsOn(buildWebApp)
+
+    doLast {
+        val srcModule   = "${serverDir}/main/x/welcome.x"
+        val resourceDir = "${serverDir}/main/resources"
+        val libDir      = "${serverDir}/build"
+
+        val src  = file("$srcModule").lastModified()
+        val rsrc = fileTree("$resourceDir").getFiles().stream().
+                mapToLong({f -> f.lastModified()}).max().orElse(0)
+        val dest = file("$libDir/welcome.xtc").lastModified()
+
+        if (src > dest || rsrc > dest) {
+            project.exec {
+                commandLine("$xdkExe/xtc", "-verbose", "-rebuild",
+                            "-o", "$libDir",
+                            "-L", "$libDir",
+                            "$srcModule")
+            }
+        }
+        else {
+            println("$libDir/welcome.xtc is up to date")
+            }
+    }
+}
+
+val compileWelcomeDB = tasks.register("compileWelcomeDB") {
+    group       = "Build"
+    description = "Compile welcome module"
+
+    val srcModule = "${serverDir}/main/x/welcomeDB.x"
+    val libDir    = "${serverDir}/build"
+
+    val src  = file("$srcModule").lastModified()
+    val dest = file("$libDir/welcomeDB.xtc").lastModified()
+
+    if (src > dest) {
+        project.exec {
+            commandLine("$xdkExe/xtc", "-verbose",
+                        "-o", "$libDir",
+                        "$srcModule")
+        }
+    }
+    else {
+        println("$libDir/welcomeDB.xtc is up to date")
+        }
+}
+
+tasks.register<Copy>("updateXdk") {
+
+    var xvmHome = System.getProperty("xvm.home")
+    if (xvmHome == null || xvmHome == "") {
+        xvmHome = "../../../xvm"
+    }
+
+    val xdkExt = "$xvmHome/xdk/build/xdk"
+    val xdkLib = "$serverDir/xdk"
+
+    val srcTimestamp = fileTree(xdkExt).getFiles().stream().
+            mapToLong({f -> f.lastModified()}).max().orElse(0)
+    val dstTimestamp = fileTree(xdkLib).getFiles().stream().
+            mapToLong({f -> f.lastModified()}).max().orElse(0)
+
+    if (srcTimestamp > dstTimestamp) {
+        from("$xdkExt") {
+            include("bin/**")
+            include("javatools/**")
+            include("lib/**")
+        }
+        into("$xdkLib")
+        doLast {
+            println("Finished task: updateXdk")
+        }
+    }
+    else {
+        println("Xdk is up to date")
+    }
+}
+
+// ----- webapp build tasks ------------------------------------------------------------------------
+
+val buildWebApp = tasks.register("buildWebApp") {
+
+    val src1 = fileTree("$webappDir/src").getFiles().stream().
+            mapToLong({f -> f.lastModified()}).max().orElse(0)
+    val src2 = fileTree("$webappDir/public").getFiles().stream().
+            mapToLong({f -> f.lastModified()}).max().orElse(0)
+    val dest = fileTree("$webContent").getFiles().stream().
+            mapToLong({f -> f.lastModified()}).max().orElse(0)
+
+    if (src1 > dest || src2 > dest) {
+        dependsOn(copyContent)
+        }
+    else {
+        println("$webContent is up to date")
+        }
+}
+
+val copyContent = tasks.register("copyContent") {
+    dependsOn(npmBuild)
+
+    doLast {
+        println("Copying static content from $$webappDir/build to $webContent")
+
+        copy {
+            from("$webappDir/build")
+            into(webContent)
+        }
+    }
+}
+
+val npmBuild = tasks.register("npmBuild") {
+    project.exec {
+        workingDir(webappDir)
+        commandLine("npm", "run", "build")
     }
 }
--- welcome/webapp/build.gradle.kts
+++ welcome/webapp/build.gradle.kts
@@ -2,43 +2,9 @@
  * Build the "webapp" content.
  */
 
-val server = project(":server")
-
-val xdkExe = "${projectDir}/xdk/bin"
-
 tasks.register("clean") {
     group       = "Build"
     description = "Delete previous build results"
 
     delete("$buildDir")
-}
-
-tasks.register("build") {
-    group       = "Build"
-    description = "BUild the webapp content"
-
-    val dirResources = "${server.projectDir}/main/resources/webapp"
-
-    val src1 = fileTree("$projectDir/src").getFiles().stream().
-            mapToLong({f -> f.lastModified()}).max().orElse(0)
-    val src2 = fileTree("$projectDir/public").getFiles().stream().
-            mapToLong({f -> f.lastModified()}).max().orElse(0)
-    val dest = fileTree("$dirResources").getFiles().stream().
-            mapToLong({f -> f.lastModified()}).max().orElse(0)
-
-    if (src1 > dest || src2 > dest) {
-        project.exec {
-            commandLine("npm",
-                        "run",
-                        "build")
-        }
-
-        doLast {
-            println("Copying static content from $buildDir to $dirResources")
-            copy {
-                from("$buildDir")
-                into(dirResources)
-            }
-        }
-    }
 }
